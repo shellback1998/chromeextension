a10949f971d885d95eb9a97493272582163a5321
Scott M Gaudet <shellback1998@gmail.com>
2026-08-08 08:18:02 -0400

    Rendered the myLeads array to the DOM in the ul-el element unordered list

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function (event) {
     myLeads.push(inputEl.value);
@@ -8,5 +9,6 @@ inputBtn.addEventListener("click", function (event) {
 })
 
 for (let i = 0; i < myLeads.length; i++) {
+    ulEl.textContent += myLeads[i] + " "
     console.log(myLeads[i])
 }
