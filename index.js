f9e4745a2ac6fff8c0b3233e8397665cfc620003
Scott M Gaudet <shellback1998@gmail.com>
2026-08-08 10:16:10 -0400

    Create a new variable listItems. Manipulating the DOM comes with a cost. So, took the innerHTLM outside of the for loop so it only runs once.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+let listItems = ""
 
 inputBtn.addEventListener("click", function (event) {
     myLeads.push(inputEl.value);
@@ -9,6 +10,8 @@ inputBtn.addEventListener("click", function (event) {
 })
 
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
+    listItems += "<li>" + myLeads[i] + "</li> "
     console.log(myLeads[i])
 }
+
+ulEl.innerHTML = listItems;
