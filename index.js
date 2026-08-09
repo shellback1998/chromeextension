592771afdaee4eaeaa5ae19638a24db0ee3f4922
Scott M Gaudet <shellback1998@gmail.com>
2026-08-09 06:58:23 -0400

    Implemented the logic to delete the local storage, the ul and the myLeads array

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-let listItems = ""
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const ulEl = document.getElementById("ul-el")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear()
+    myLeads = []
+    ulEl.innerHTML = ""
+})
 
 inputBtn.addEventListener("click", function (event) {
     myLeads.push(inputEl.value);
