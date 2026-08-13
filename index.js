ef6a48fa0137c39b58a1e64ebdee939bc071841b
Scott M Gaudet <shellback1998@gmail.com>
2026-08-12 20:26:57 -0400

    Updated the listener to use the Chrome Tab which is active and the active window

--- index.js
+++ index.js
@@ -11,18 +11,14 @@ if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
 
 tabBtn.addEventListener("click", function(){
-    // Save the url instead of logging it out
-    console.log(tabs[0].url)
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
-
 
 function render(leads) {
     let listItems = ""
